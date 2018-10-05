c82bca7882fa943e6716ae9b0630c38609d68884
kjmkagaya <jm.kagaya@gmail.com>
2018-10-05 09:42:53 +0900

    Done
    Fix some bug

--- 01_Laravel_5.7_Generate_PDF_From_HTML/blog/app/Http/Controllers/HomeController.php
+++ 01_Laravel_5.7_Generate_PDF_From_HTML/blog/app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use PDF;
 class HomeController extends Controller
 {
     //
@@ -12,8 +12,9 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function generatePDF()
-    {
+
+    public function generatePDF(){ 
+    
         $data = ['title' => 'Welcome to HDTuto.com'];
         $pdf = PDF::loadView('myPDF', $data);
   
