76b3639374ebbbe5dbaf8300b93b7cfb96137a0f
kjmkagaya <jm.kagaya@gmail.com>
2018-10-11 10:06:18 +0900

    Done
    Done

--- 07_Jquery_Form_Validation/blog/routes/web.php
+++ 07_Jquery_Form_Validation/blog/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('validation','FormController@validation');
+Route::post('validation','FormController@validationPost');
+
