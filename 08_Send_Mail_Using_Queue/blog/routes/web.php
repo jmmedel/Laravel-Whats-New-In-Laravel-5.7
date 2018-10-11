ee6e9be86e5aa406655934eb673fc759aba6e415
kjmkagaya <jm.kagaya@gmail.com>
2018-10-11 10:52:18 +0900

    Done
    Done

--- 08_Send_Mail_Using_Queue/blog/routes/web.php
+++ 08_Send_Mail_Using_Queue/blog/routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('email-test', function(){
+  
+	$details['email'] = 'your_email@gmail.com';
+  
+    dispatch(new App\Jobs\SendEmailJob($details));
+  
+    dd('done');
+});
+
